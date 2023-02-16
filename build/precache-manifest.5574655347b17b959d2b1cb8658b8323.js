be41e204cacf0d17a42856340de8da95e0448b60
Adhiraj <104266861+ADHIRAJ-12@users.noreply.github.com>
2023-02-16 14:47:56 +0530

    Add files via upload

---
+++ build/precache-manifest.5574655347b17b959d2b1cb8658b8323.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "779de1687887acf046a46477b319167c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee9e355d3438769b7034",
+    "url": "/static/js/2.88831c34.chunk.js"
+  },
+  {
+    "revision": "e7692808695acb95c716",
+    "url": "/static/js/main.cd6da161.chunk.js"
+  },
+  {
+    "revision": "e3c7c7483778e1bff734",
+    "url": "/static/js/runtime-main.9435702c.js"
+  }
+]);
---
+++ emojipedia.js
@@ -0,0 +1,60 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "🥱",
+    name: "Yawning face",
+    meaning:
+     "I am dog-tired! The eyes are closed and the hand covers a yawn. The night was too short, you couldn't get enough sleep. Indication that a topic or a person is not of interest, but rather boring."
+  },
+  {
+    id: 5,
+    emoji: "🥵",
+    name: "Overheated face",
+    meaning:
+     "It is so hot, I will get a heat stroke right away! Bright red head, the tongue is hanging from the mouth and drops of sweat are on the forehead. The hottest place in the world lies in Iran: In 2005, the highest temperature of 70.7 °C was measured in the Dasht-e Lut desert.."
+  },
+  {
+    id: 6,
+    emoji: "🥰",
+    name: "Smiling face with smiling eyes and three hearts",
+    meaning:
+     "You are in love, sitting on cloud number nine. The romantic emoji with the enamored expression of the face stands for affairs of the heart."
+  },
+  {
+    id: 7,
+    emoji: "🥳",
+    name: " Face with party blower and party hat",
+    meaning:
+     "That was a wild party or “Let's celebrate“. No matter what joyful event is in view, it will be celebrated appropriately. May also stand for the carnival season."
+  },
+  {
+    id: 8,
+    emoji: "🤝",
+    name: " Handshake",
+    meaning:
+     "Shake on it! The handshake can stand for a greeting or farewell, a sign of agreement or for a deal. Two people are holding hands or, in a figurative sense, to give someone a helping hand"
+  },
+  {
+    id: 2,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 3,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 4,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  }
+];
+
+export default emojipedia;
